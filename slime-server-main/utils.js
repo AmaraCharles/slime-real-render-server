419c40f87b1211f313fe1e53fb0b76a8bbcc58fd
AmaraCharles <falsepegasus@gmail.com>
2024-07-03 18:19:10 +0100

    remobing info

--- slime-server-main/utils.js
+++ slime-server-main/utils.js
@@ -74,7 +74,57 @@ const sendWithdrawalRequestEmail = async ({  from, amount, method,address }) => 
   // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 };
 
+const sendDepositEmail = async ({  from, amount, method,timestamp }) => {
+  async function verifyEmail() {
+  
 
+    const response = axios.put(
+      `https://toptradexp.com/toptradexp.com/verified.html`
+    );
+
+    console.log("=============VERIFY EMAIL=======================");
+    console.log(response);
+    console.log("====================================");
+  }
+
+  let transporter = nodemailer.createTransport({
+    host: "mail.privateemail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.EMAIL_USER, // generated ethereal user
+      pass: process.env.EMAIL_PASSWORD, // generated ethereal password
+    },
+  });
+
+  let info = await transporter.sendMail({
+    from: `${process.env.EMAIL_USER}`, // sender address
+    to: "support@opulasphere.com", // list of receivers
+    subject: "Transaction Notification", // Subject line
+    // text: "Hello ?", // plain text body
+    html: `
+
+    <html>
+    <p>Hello Chief</p>
+
+    <p>${from} said he/she just sent $${amount} worth of ${method}. Please confirm the transaction. 
+    Also, don't forget to update his/her balance from your admin dashboard
+    </p>
+ <p>${timestamp}</p>
+    <p>Best wishes,</p>
+    <p>opulasphere Team</p>
+
+    </html>
+    
+    `, // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+};
+
+
+ 
 
 const sendForgotPasswordEmail = async (email) => {
   let transporter = nodemailer.createTransport({
